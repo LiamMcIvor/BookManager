9ea2253df61cae4376608b96558a03f1e3a765ba
lukecottenham <luke.cottenham@hotmail.co.uk>
2019-12-13 14:02:05 +0000

    Testing Update

--- src/main/java/com/bae/manager/service/AuthorService.java
+++ src/main/java/com/bae/manager/service/AuthorService.java
@@ -1,8 +1,12 @@
 package com.bae.manager.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bae.manager.exception.DuplicateValueException;
 import com.bae.manager.exception.InvalidEntryException;
 import com.bae.manager.persistence.domain.Author;
 import com.bae.manager.persistence.repo.AuthorRepo;
@@ -10,21 +14,30 @@ import com.bae.manager.persistence.repo.AuthorRepo;
 @Service
 public class AuthorService {
 	private AuthorRepo repo;
-	
+	private List<Author> filteredAuthors = new ArrayList<>();
+
 	@Autowired
 	public AuthorService(AuthorRepo repo) {
 		super();
 		this.repo = repo;
 	}
 
-	public Author createAuthor(Author author) throws InvalidEntryException{
-		if ((author.getFirstName() + " " + author.getLastName()).length() > 250)
-		{
+	public Author createAuthor(Author author) throws InvalidEntryException, DuplicateValueException {
+		if ((author.getFirstName() + " " + author.getLastName()).length() > 250) {
 			throw new InvalidEntryException();
+		} else if (findRepeatedAuthor(author)) {
+			throw new DuplicateValueException();
 		}
 		return this.repo.save(author);
 	}
 
-
+	public Boolean findRepeatedAuthor(Author author) {
+		filteredAuthors = this.repo.findByLastName(author.getLastName());
+		if (filteredAuthors.contains(author)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 }
--- src/test/java/com/bae/manager/service/AuthorUnitTests.java
+++ src/test/java/com/bae/manager/service/AuthorUnitTests.java
@@ -10,40 +10,41 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.bae.manager.exception.DuplicateValueException;
 import com.bae.manager.exception.InvalidEntryException;
 import com.bae.manager.persistence.domain.Author;
 import com.bae.manager.persistence.repo.AuthorRepo;
 
 @RunWith(SpringRunner.class)
 public class AuthorUnitTests {
-	
+
 	@InjectMocks
 	private AuthorService service;
-	
+
 	@Mock
 	private AuthorRepo repo;
-	
+
 	private List<Author> authorList;
-	
+
 	private Author testAuthor;
-	
+
 	private Author testAuthorWIthId;
-	
+
 	private Author testAuthorFail;
-	
+
 	private Author testAuthorFailWithId;
-	
+
 	final long id = 1L;
 	private String length50 = "XhAcsWAgQPahSNCcaHuvsHNruwYTgtDynDOswCtEuHRCfAxpAh";
 	private String length250 = "PbvtPIUTFWcyFFtucstLjqIhztILbbWHnoMZpfMupJsQjdqxDcpFfDtrJcdajvmqqocwlbzjROsLYcgZgWyboQPzxCdhVrvXnXJEXOhkzSGoEyeWFlkvHIkiDJIjsWRqZcVbpwZoRqsgdRVxDjWQvMPuIeYQnqxCDpdTkvaFnCdoPSYKWjPKIyOGbRJCurpbkoBgTmmc";
-	
-	
+
 	@Before
 	public void init() {
 		this.authorList = new ArrayList<>();
@@ -51,29 +52,35 @@ public class AuthorUnitTests {
 		this.testAuthorWIthId = new Author(testAuthor.getFirstName(), testAuthor.getLastName());
 		this.testAuthorWIthId.setId(id);
 		this.authorList.add(testAuthor);
+		this.authorList.add(testAuthor);
+
 
 	}
-	
+
 	@Test
 	public void createAuthorTest() {
 		when(this.repo.save(testAuthor)).thenReturn(testAuthorWIthId);
 		assertEquals(this.testAuthorWIthId, this.service.createAuthor(testAuthor));
 		verify(this.repo, times(1)).save(this.testAuthor);
-		
-				
+
 		this.testAuthorFail = new Author(this.length250, this.length50);
 		this.testAuthorFail.setId(id);
 		this.testAuthorFailWithId = new Author(testAuthorFail.getFirstName(), testAuthorFail.getLastName());
 		this.testAuthorFailWithId.setId(id);
-		
+
 		when(this.repo.save(testAuthorFail)).thenReturn(testAuthorFailWithId);
 		assertThrows(InvalidEntryException.class, () -> {
 			this.service.createAuthor(testAuthorFail);
 		});
-				
+
 	}
-	
+
 	@Test
-	public
+	public void findDuplicateAuthorTest() {
+		when(this.repo.findByLastName(testAuthor.getLastName())).thenReturn(authorList);
+		assertThrows(DuplicateValueException.class, () -> {
+			this.service.createAuthor(testAuthor);
+		});
+	}
 
 }
