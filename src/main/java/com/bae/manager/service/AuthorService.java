ed764e78e8f4632703d9992ae33d30d554decdbb
lukecottenham <luke.cottenham@hotmail.co.uk>
2019-12-13 12:49:27 +0000

    createAuthor Test

--- src/main/java/com/bae/manager/service/AuthorService.java
+++ src/main/java/com/bae/manager/service/AuthorService.java
@@ -3,6 +3,7 @@ package com.bae.manager.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bae.manager.exception.InvalidEntryException;
 import com.bae.manager.persistence.domain.Author;
 import com.bae.manager.persistence.repo.AuthorRepo;
 
@@ -16,7 +17,11 @@ public class AuthorService {
 		this.repo = repo;
 	}
 
-	public Author createAuthor(Author author) {
+	public Author createAuthor(Author author) throws InvalidEntryException{
+		if ((author.getFirstName() + " " + author.getLastName()).length() > 250)
+		{
+			throw new InvalidEntryException();
+		}
 		return this.repo.save(author);
 	}
 
