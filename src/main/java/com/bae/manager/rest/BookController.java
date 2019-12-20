0163f27dcf94ffec9eaed21f5eca804b639f8a84
lukecottenham <luke.cottenham@hotmail.co.uk>
2019-12-20 13:34:28 +0000

    create book test

--- src/main/java/com/bae/manager/rest/BookController.java
+++ src/main/java/com/bae/manager/rest/BookController.java
@@ -1,5 +1,24 @@
 package com.bae.manager.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bae.manager.service.BookService;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+	
+	private BookService service;
+	
+	@Autowired
+	public BookController(BookService service) {
+		super();
+		this.service = service;
+	}
+	
+	
 
 }
--- src/test/java/com/bae/manager/rest/BookControllerUnitTest.java
+++ src/test/java/com/bae/manager/rest/BookControllerUnitTest.java
@@ -1,14 +1,55 @@
 package com.bae.manager.rest;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import com.bae.manager.enums.Completion;
+import com.bae.manager.enums.Owned;
+import com.bae.manager.persistence.domain.Book;
+import com.bae.manager.service.BookService;
 
 public class BookControllerUnitTest {
+	
+	@InjectMocks
+	private BookController controller;
+	
+	@Mock
+	private BookService service;
+	
+	private List<Book> bookList;
+	
+	private Book testBook;
+	
+	private Book testBookWithId;
+	
+	private final long id = 1;
+	
+	@Before
+	public void init() {
+		this.bookList = new ArrayList<>();
+		this.testBook = new Book("The Colour of Magic", "9780061685965", "Discworld", 2, Owned.OWNED, Completion.READING);
+		this.testBookWithId = new Book(testBook.getTitle(), testBook.getIsbn(), testBook.getSeries(), testBook.getTimesRead(), testBook.getOwned(), testBook.getCompletion());
+		this.testBookWithId.setId(id);
+		this.bookList.add(testBook);
+	}
+	
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createBookTest() {
+		when(this.service.createBook(testBook)).thenReturn(this.testBookWithId);
+		assertEquals(this.testBookWithId, this.service.createBook(testBook));
+		verify(this.service, times(1)).createBook(testBook);		
 	}
 
 }
