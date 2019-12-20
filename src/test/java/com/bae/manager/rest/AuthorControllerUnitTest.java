dac822f75936aae054886deda2229797907cf75a
lukecottenham <luke.cottenham@hotmail.co.uk>
2019-12-20 12:49:14 +0000

    Create Author test

--- src/test/java/com/bae/manager/rest/AuthorControllerUnitTest.java
+++ src/test/java/com/bae/manager/rest/AuthorControllerUnitTest.java
@@ -1,14 +1,55 @@
 package com.bae.manager.rest;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.bae.manager.persistence.domain.Author;
+import com.bae.manager.service.AuthorService;
+
+@RunWith(SpringRunner.class)
 public class AuthorControllerUnitTest {
+	
+	@InjectMocks
+	private AuthorController controller;
+	
+	@Mock
+	private AuthorService service;
+	
+	private List<Author> authorList;
+	
+	private Author testAuthor;
+	
+	private Author testAuthorWithId;
+	
+	private final long id = 1L;
+	
+	@Before
+	public void init() {
+		this.authorList = new ArrayList<>();
+		this.testAuthor = new Author("Terry Pratchett");
+		this.authorList.add(testAuthor);
+		this.testAuthorWithId = new Author(testAuthor.getPenName());
+		this.testAuthorWithId.setId(this.id);
+		
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void createAuthorTest() {
+		when(this.service.createAuthor(testAuthor)).thenReturn(testAuthorWithId);
+		assertArrayEquals(this.testAuthorWithId, this.controller.createAuthor(testAuthor));
+		verify(this.service, times(1)).createAuthor(this.testAuthor);
 	}
 
 }
