ec2142dba7184bac1fc538e3cb015aec3a83cdff
lukecottenham <luke.cottenham@hotmail.co.uk>
2019-12-20 11:39:23 +0000

    Test and controller class creation

---
+++ src/test/java/com/bae/manager/rest/BookControllerUnitTest.java
@@ -0,0 +1,14 @@
+package com.bae.manager.rest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookControllerUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
