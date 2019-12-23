5cd1eebf78637745042fda67a5fa4d1c6113e95d
Luke <Luke@AsusPC>
2019-12-23 16:14:00 +0000

    update book test pass

--- src/main/java/com/bae/manager/rest/BookController.java
+++ src/main/java/com/bae/manager/rest/BookController.java
@@ -2,12 +2,15 @@ package com.bae.manager.rest;
 
 import java.util.List;
 
+import javax.websocket.server.PathParam;
+
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +48,11 @@ public class BookController {
 	@DeleteMapping("/delete/{id}")
 	public Boolean deleteBook(@PathVariable Long id) {
 		return this.service.deleteBook(id);
+	}
+
+	@PutMapping("updateBook")
+	public Book updateBook(@RequestBody Book book, @PathParam("id") Long id) {
+		return this.service.updateBook(book, id);
 	}
 	
 }
