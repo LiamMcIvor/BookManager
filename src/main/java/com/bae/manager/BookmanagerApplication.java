78641be99dba715deecae005f424d785e4699066
Luke <luke.cottenham@hotmail.co.uk>
2020-01-07 10:41:10 +0000

    updates complete

--- src/main/java/com/bae/manager/BookmanagerApplication.java
+++ src/main/java/com/bae/manager/BookmanagerApplication.java
@@ -2,12 +2,19 @@ package com.bae.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookmanagerApplication {
+public class BookmanagerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookmanagerApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BookmanagerApplication.class);
 	}
 
 }
