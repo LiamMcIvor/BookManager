b3da9713974cb87b8901668e46013f4d483303d3
qa-admin <qa-admin@qaadmin-HP-ProDesk-400-G3-SFF>
2020-01-10 14:48:52 +0000

    Several integration tests written and passed for bok services

--- src/test/java/com/bae/manager/service/BookServiceIntegrationTests.java
+++ src/test/java/com/bae/manager/service/BookServiceIntegrationTests.java
@@ -1,12 +1,17 @@
 package com.bae.manager.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Before;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.bae.manager.enums.Completion;
@@ -15,26 +20,73 @@ import com.bae.manager.persistence.domain.Book;
 import com.bae.manager.persistence.repo.BookRepo;
 
 @RunWith(SpringRunner.class)
-@DataJpaTest
-class BookServiceIntegrationTests {
+@SpringBootTest
+public class BookServiceIntegrationTests {
+	
 	@Autowired
 	private BookRepo repo;
-		
-	private final Book TEST_BOOK = new Book("The Colour of Magic", "Discworld", 2, Owned.OWNED, Completion.READING);
-	
-	private Book testSavedBook;
 
+	@Autowired
+	private BookService service;
+	
+	private Book testBook;
+	
+	private Book testBookWithid;
+	
+	private Book testBook2;
+	
+	private Book testBook2Withid;
+	
+	private final Long id = 2L;
+	
+	public BookServiceIntegrationTests() {
+	}
+	
 	@Before
-	void init() {
+	public void init() {
+		this.testBook = new Book("The Colour of Magic", "Discworld", 2, Owned.OWNED, Completion.READING);
+		this.testBook2 = new Book("Good Omens", "N/A", 0, Owned.WISHLIST, Completion.TO_READ);
+		this.testBook2Withid = new Book(this.testBook2.getTitle(), this.testBook2.getSeries(), this.testBook2.getTimesRead(), this.testBook2.getOwned(), this.testBook2.getCompletion());
+		this.testBook2Withid.setId(this.id);
 		this.repo.deleteAll();
-		this.testSavedBook = this.repo.save(this.TEST_BOOK);
-		
+		this.testBookWithid = this.repo.save(this.testBook);		
 	}
 	
 	@Test
-	public void integrationTest() {
-		assertThat(this.repo.findById(1L)).contains(this.testSavedBook);
+	public void createBookTest() {
+		assertEquals(this.testBook2Withid, this.service.createBook(this.testBook2));
 	}
 	
+	@Test
+	public void deleteBookTest() {
+		assertThat(this.service.deleteBook(this.testBookWithid.getId())).isFalse();
+	}
+	
+	@Test
+	public void findBookByIdTest() {
+		assertEquals(this.testBookWithid, this.service.findBookById(this.testBookWithid.getId()));
+	}
+	
+	@Test
+	public void getBooksTest() {
+		this.testBook2Withid = this.service.createBook(testBook2);
+		assertEquals(Arrays.asList((new Book[] {this.testBookWithid, this.testBook2Withid})), this.service.getAllBooks());
+	}
+	
+	@Test
+	public void findRepeatedBookTest() {
+		assertTrue(this.service.findRepeatedBook(testBook));
+	}
+	
+	@Test
+	public void updateBookTest() {
+		Book updatedBook = new Book(this.testBook2.getTitle(), this.testBook2.getSeries(), this.testBook2.getTimesRead(), this.testBook2.getOwned(), this.testBook2.getCompletion());
+		updatedBook.setId(testBookWithid.getId());
+		
+		assertEquals(updatedBook, this.service.updateBook(testBook2, this.testBookWithid.getId()));
+	}
+	
+	@Test
+	public void verifyValidBook
 
 }
